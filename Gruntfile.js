2be05b4ffded74e63dd40e6385c06e19dd13d6a0
jed apostol <jeda@mindtouch.com>
2014-06-22 17:38:19 -0700

    Updated grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,21 +45,6 @@ module.exports = function(grunt) {
             },
         },
 
-        // Clean Config
-        clean: {
-            dist: {
-                files: [ {
-                    dot: true,
-                    src: [
-                        '.tmp',
-                        'dist/*',
-                        '!dist/.git*'
-                    ]
-                } ]
-            },
-            server: [ '.tmp' ],
-        },
-
         // Hint Config
         jshint: {
             options: {
@@ -113,143 +98,7 @@ module.exports = function(grunt) {
                 path: './',
                 app: 'Atom'
             },
-        },
-
-        // Rev Config
-        rev: {
-            dist: {
-                files: {
-                    src: [
-                        'dist/assets/scripts/**/*.js',
-                        'dist/assets/styles/**/*.css',
-                        'dist/assets/images/**/*.{png,jpg,jpeg,gif,webp}',
-                        'dist/assets/styles/fonts/**/*.*'
-                    ]
-                }
-            }
-        },
-
-        // Usemin Config
-        useminPrepare: {
-            options: {
-                dest: 'dist/assets'
-            },
-            html: [ 'assets/{,*/}*.html', 'views/**/*.handlebars' ]
-        },
-        usemin: {
-            options: {
-                dirs: ['dist/assets'],
-                basedir: 'dist/assets',
-            },
-            html: [ 'dist/assets/{,*/}*.html', 'dist/views/**/*.handlebars' ],
-            css: [ 'dist/assets/styles/{,*/}*.css' ]
-        },
-
-        // Imagemin Config
-        imagemin: {
-            dist: {
-                files: [ {
-                    expand: true,
-                    cwd: 'assets/images',
-                    src: '**/*.{png,jpg,jpeg}',
-                    dest: 'dist/assets/images'
-                } ]
-            }
-        },
-
-        // SVGmin Config
-        svgmin: {
-            dist: {
-                files: [ {
-                    expand: true,
-                    cwd: 'assets/images',
-                    src: '{,*/}*.svg',
-                    dest: 'dist/assets/images'
-                } ]
-            }
-        },
-
-        // CSSmin config
-        cssmin: {
-            // This task is pre-configured if you do not wish to use Usemin
-            // blocks for your CSS. By default, the Usemin block from your
-            // `index.html` will take care of minification, e.g.
-            //
-            //     <!-- build:css({.tmp,app}) styles/main.css -->
-            //
-            // dist: {
-            //     files: {
-            //         'dist/assets/styles/main.css': [
-            //             '.tmp/styles/{,*/}*.css',
-            //             'assets/styles/{,*/}*.css'
-            //         ]
-            //     }
-            // }
-        },
-
-        // HTML Config
-        htmlmin: {
-            dist: {
-                options: {
-                    /*removeCommentsFromCDATA: true,
-                    // https://github.com/yeoman/grunt-usemin/issues/44
-                    //collapseWhitespace: true,
-                    collapseBooleanAttributes: true,
-                    removeAttributeQuotes: true,
-                    removeRedundantAttributes: true,
-                    useShortDoctype: true,
-                    removeEmptyAttributes: true,
-                    removeOptionalTags: true*/
-                },
-                files: [ {
-                    expand: true,
-                    cwd: 'assets',
-                    src: '*.html',
-                    dest: 'dist/assets'
-                } ]
-            }
-        },
-
-        // Copy Config
-        // Put files not handled in other tasks here
-        copy: {
-            dist: {
-                files: [ {
-                    expand: true,
-                    dot: true,
-                    cwd: 'assets',
-                    dest: 'dist/assets',
-                    src: [
-                        '*.{ico,png,txt}',
-                        '.htaccess',
-                        'images/**/*.{webp,gif}',
-                        'styles/fonts/{,*/}*.*',
-                    ]
-                }, {
-                    expand: true,
-                    dot: true,
-                    cwd: 'views',
-                    dest: 'dist/views/',
-                    src: '**/*.handlebars',
-                } ]
-            },
-            styles: {
-                expand: true,
-                dot: true,
-                cwd: 'assets/styles',
-                dest: '.tmp/styles/',
-                src: '{,*/}*.css'
-            },
-        },
-
-        // Concurrent Config
-        concurrent: {
-            dist: [
-                'copy:styles',
-                'svgmin',
-                'htmlmin'
-            ]
-        },
+        }
     });
 
     // Register Tasks
@@ -265,20 +114,6 @@ module.exports = function(grunt) {
     grunt.registerTask('restart', 'Restart the server.', [
         'express:dev',
         'watch'
-    ]);
-
-    // Build
-    grunt.registerTask('build', 'Build production ready assets and views.', [
-        'clean:dist',
-        'concurrent:dist',
-        'useminPrepare',
-        'imagemin',
-        'concat',
-        'cssmin',
-        'uglify',
-        'copy:dist',
-        'rev',
-        'usemin',
     ]);
 
 };
